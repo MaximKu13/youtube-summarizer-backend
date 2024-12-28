9cfe5d55634c3b344f7ec49c77844cf8c319b20a
MaximKu13 <mkoutasov@yahoo.com>
2024-12-28 10:54:55 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
   apiUrl: process.env.NODE_ENV === 'production' 
-    ? 'https://your-vercel-app-name.vercel.app/api' // Replace with your actual Vercel backend URL
+    ? 'youtube-summarizer-backend-psi.vercel.app' // Replace with your actual Vercel backend URL
     : 'http://localhost:3000/api'
 };
 
